6a5c2adf2b9cbbfb2e0c5fd16430c051d90b08c4
Muskan Gupta <muskangupta79859@gmail.com>
2026-01-12 21:34:02 +0530

    Added Python basics code

---
+++ day1_basics.py
@@ -0,0 +1,14 @@
+# variables and data types
+name="Muskan"
+age=20
+is_student=True
+print("Name:",name)
+print("Age:",age)
+print("Is Student",is_student)
+#Arithmatic Operations
+a=10
+b=5
+print("Addition:",a + b)
+print("Subtraction:",a - b)
+print("Multiplication:",a * b)
+print("Division:",a / b)
