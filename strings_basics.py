b7eab50bee7b0dd7b0289b1e391e507054ce7128
Muskan Gupta <muskangupta79859@gmail.com>
2026-01-13 22:04:01 +0530

    Added strings basics examples

---
+++ strings_basics.py
@@ -0,0 +1,16 @@
+#String Basics
+name="Muskan"
+print(name)
+#Indexing
+print(name[2])
+#slicing
+print(name[0:3])
+#Length
+print(len(name))
+#Upper and Lower
+print(name.upper())
+print(name.lower())
+#Replace
+print(name.replace("M","N"))
+#Check word
+print("kan" in name)
