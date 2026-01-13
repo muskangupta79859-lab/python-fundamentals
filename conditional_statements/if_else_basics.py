8f1b32bd3299e66285b6594cf53fdad261c94c9f
Muskan Gupta <muskangupta79859@gmail.com>
2026-01-13 22:14:15 +0530

    Added basics if-else programs

---
+++ conditional_statements/if_else_basics.py
@@ -0,0 +1,11 @@
+#If-Else Examples
+num=int(input("Enter a number: "))
+if num%2==0:
+  print("Even number")
+else:
+  print("Odd number")
+age=int(input("Enter your age: "))
+if age>=18:
+  print("Eligible for voting")
+else:
+  print("Not eligible for voting")
