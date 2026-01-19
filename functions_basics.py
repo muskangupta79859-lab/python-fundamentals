1982ac9172ae5eb8c8dcda2bafef07cf3d018762
Muskan Gupta <muskangupta79859@gmail.com>
2026-01-19 21:39:30 +0530

    Added basic functions for greeting, addition, and even/odd check

---
+++ functions_basics.py
@@ -0,0 +1,17 @@
+# Function Basics
+
+def greet(name):
+  print("hello",name)
+
+def add(a,b):
+  print("Sum = ",a+b)
+
+def even_odd(num):
+  if num % 2 ==0:
+    print("Even Number")
+  else:
+    print("Odd Number")
+
+greet("User")
+add(5,7)
+even_odd(10)
