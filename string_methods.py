976c9b905ed10c6850b74a7625d295ff1746985a
Muskan Gupta <muskangupta79859@gmail.com>
2026-01-25 22:23:45 +0530

    Added string manipulation methods in string_methods.py

---
+++ string_methods.py
@@ -0,0 +1,28 @@
+text = " Python Programming Language "
+
+print("Original String:")
+print(text)
+
+# Convert to lowercase
+print("\nLowercase:")
+print(text.lower())
+
+# Convert to uppercase
+print("\nUppercase:")
+print(text.upper())
+
+# Remove extra spaces
+print("\nAfter strip:")
+print(text.strip())
+
+# Replace word
+print("\nAfter replace:")
+print(text.replace("Python","Core Python"))
+
+# Split string
+print("\nAfter split:")
+print(text.split())
+
+# Length of string
+print("\nLength of string:")
+print(len(text))
