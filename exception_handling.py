757f92d0cb82afa2f2e3003b3c3e3d274bdcb4f9
Muskan Gupta <muskangupta79859@gmail.com>
2026-02-02 21:27:19 +0530

    Create exception_handling.py

---
+++ exception_handling.py
@@ -0,0 +1,18 @@
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    result = num1 / num2
+    print("Result:", result)
+
+except ZeroDivisionError:
+    print("Error: Cannot divide by zero")
+
+except ValueError:
+    print("Error: Please enter valid numbers")
+
+else:
+    print("Calculation successful")
+
+finally:
+    print("Program finished")
